659952d39d9c7f18deb824354ea45c48e1709744
Emranul Hoque <emranrakib01862@gmail.com>
2025-12-19 19:33:58 +0600

    row column

---
+++ lib/home.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('This is Appbar'),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+      ),
+
+
+        body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+                Text('This is body',style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.green
+                ),),
+              ],
+            ),
+          ),
+
+      
+      
+      
+      
+      
+      
+      
+      
+      /*body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+            Text('This is body',style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+                color: Colors.green
+            ),),
+          ],
+        ),
+      )
+*/
+
+    );
+  }
+}
