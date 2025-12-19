ff10e9038e8f03f3ee4b9ec41eb4538d0067b4fe
Emranul Hoque <emranrakib01862@gmail.com>
2025-12-19 22:39:04 +0600

    add class3

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_projects/class_3.dart';
 import 'package:flutter_projects/home.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Emranul Hoque',
-      home: Home(),
+      home: Class3(),
     );
   }
 }
---
+++ lib/class_3.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Class3 extends StatelessWidget {
+  const Class3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+      //backgroundColor: Colors.grey[300],
+      //backgroundColor: Colors.grey.withOpacity(0.5),
+      backgroundColor: Colors.grey.shade200,
+    );
+  }
+}
+
