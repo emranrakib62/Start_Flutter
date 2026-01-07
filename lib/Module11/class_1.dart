0de0af440d6336c2544e13e2f9e0a17cc72b2680
Emranul Hoque <emranrakib01862@gmail.com>
2026-01-07 22:10:44 +0600

    updated

--- lib/Module11/class_1.dart
+++ lib/Module11/class_1.dart
@@ -7,12 +7,26 @@ class Module11Class1 extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
 body: SafeArea(
     child: Column(
 children: [
 
   TextField(
-
+decoration: InputDecoration(
+  hintText: 'Enter your number',
+  hintStyle: TextStyle(
+    fontSize: 18
+  ),
+  labelText: 'Phone number',
+  labelStyle: TextStyle(
+    fontSize: 18
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(50)
+  )
+    
+),
 
   )
 ]
