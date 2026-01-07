2588c8f83418018f32c9415e72848dadf7b4e758
Emranul Hoque <emranrakib01862@gmail.com>
2026-01-07 21:38:35 +0600

    updated

--- lib/Module11/class_1.dart
+++ lib/Module11/class_1.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Module11Class1 extends StatelessWidget{
   const Module11Class1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+body: SafeArea(
+    child: Column(
+children: [
+
+  TextField(
+
+
+  )
+]
+    )),
+
+    );
 
   }
 }
