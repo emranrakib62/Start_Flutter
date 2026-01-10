c4b21f0cb97f4c1c7dd37f6431413239b4c7c24a
Emranul Hoque <emranrakib01862@gmail.com>
2026-01-10 22:12:21 +0530

    updated

---
+++ lib/Module11/Class_2.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Module11Class2 extends StatelessWidget{
+  const Module11Class2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Login'),
+      ),
+      body: SafeArea(
+        child: Column(
+
+
+        children: [
+          Image.network('https://img.freepik.com/free-photo/waterfall-chae-son-national-park-lampang-thailand_554837-639.jpg?semt=ais_hybrid&w=740&q=80')
+        ],
+      ),
+    ));
+  }
+}
