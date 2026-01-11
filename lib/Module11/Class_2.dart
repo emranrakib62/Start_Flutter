6b45c6caad094a7d6392a1831ab7b8903379245d
Emranul Hoque <emranrakib01862@gmail.com>
2026-01-11 12:02:00 +0600

    updated

--- lib/Module11/Class_2.dart
+++ lib/Module11/Class_2.dart
@@ -5,9 +5,10 @@ class Module11Class2 extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+TextEditingController phoneController=TextEditingController();
+TextEditingController passwardController=TextEditingController();
 
-
-
+final formkey=GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,
@@ -17,57 +18,62 @@ class Module11Class2 extends StatelessWidget{
        child: Padding(
           padding: const EdgeInsets.all(10.0),
 
-        child: Column(
-
-
-        children: [
-          Image.network('https://img.freepik.com/free-photo/waterfall-chae-son-national-park-lampang-thailand_554837-639.jpg?semt=ais_hybrid&w=740&q=80',
-            height: 200,
-            width: 200,
-
-          ),
-SizedBox(height: 20,),
-Image.asset('assets/rakib.jpg',
-  height: 200,
-  width: 300,
-),
-          Text('Login with phone and passwoard',
-
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.w500
-          ),
-          ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+          
+          
+          children: [
+            Image.network('https://img.freepik.com/free-photo/waterfall-chae-son-national-park-lampang-thailand_554837-639.jpg?semt=ais_hybrid&w=740&q=80',
+              height: 200,
+              width: 200,
+          
+            ),
           SizedBox(height: 20,),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Phone number',
-                  border: OutlineInputBorder(),
-            ),
+          Image.asset('assets/rakib.jpg',
+            height: 200,
+            width: 300,
           ),
-SizedBox(height: 20,),
-          TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: 'passward',
-              border: OutlineInputBorder(),
+            Text('Login with phone and passwoard',
+          
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500
             ),
-          ),
-          SizedBox(height: 10,),
-
-           SizedBox(
-             width: 200,
-
-           child: ElevatedButton(
-
-             style: ElevatedButton.styleFrom(
-                 backgroundColor: Colors.orange,
-               foregroundColor: Colors.white,
-             ),
-               onPressed: (){}, child: Text('Login')
-           ))
-        ],
-      ),),
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              controller: phoneController,
+              decoration: InputDecoration(
+                hintText: 'Phone number',
+                    border: OutlineInputBorder(),
+              ),
+            ),
+          SizedBox(height: 20,),
+            TextFormField(
+              obscureText: true,
+              controller: passwardController,
+              decoration: InputDecoration(
+                hintText: 'passward',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10,),
+          
+             SizedBox(
+               width: 200,
+          
+             child: ElevatedButton(
+          
+               style: ElevatedButton.styleFrom(
+                   backgroundColor: Colors.orange,
+                 foregroundColor: Colors.white,
+               ),
+                 onPressed: (){}, child: Text('Login')
+             ))
+          ],
+                ),
+        ),),
     ));
   }
 }
