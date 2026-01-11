4fe11ce94b1625f35e2534b1089760ee1640fbc3
Emranul Hoque <emranrakib01862@gmail.com>
2026-01-11 11:30:16 +0600

    updated

--- lib/Module11/Class_2.dart
+++ lib/Module11/Class_2.dart
@@ -18,8 +18,50 @@ class Module11Class2 extends StatelessWidget{
 
 
         children: [
-          Image.network('https://img.freepik.com/free-photo/waterfall-chae-son-national-park-lampang-thailand_554837-639.jpg?semt=ais_hybrid&w=740&q=80'),
+          Image.network('https://img.freepik.com/free-photo/waterfall-chae-son-national-park-lampang-thailand_554837-639.jpg?semt=ais_hybrid&w=740&q=80',
+            height: 200,
+            width: 200,
 
+          ),
+SizedBox(height: 20,),
+Image.asset('assets/rakib.jpg',
+  height: 200,
+  width: 300,
+),
+          Text('Login with phone and passwoard',
+
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w500
+          ),
+          ),
+          SizedBox(height: 20,),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Phone number',
+                  border: OutlineInputBorder(),
+            ),
+          ),
+SizedBox(height: 20,),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'passward',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 20,),
+
+           SizedBox(
+             width: 200,
+
+           child: ElevatedButton(
+             style: ElevatedButton.styleFrom(
+                 backgroundColor: Colors.orange,
+               foregroundColor: Colors.white,
+             ),
+               onPressed: (){}, child: Text('Login')
+           ))
         ],
       ),
     ));
