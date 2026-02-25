5587c2be0e8e21d6d82db1838697c4eda60bbf25
Emranul Hoque <emranrakib01862@gmail.com>
2026-02-25 11:46:38 +0600

    updated

--- lib/Module13/class_1.dart
+++ lib/Module13/class_1.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Widget/City_card.dart';
+
 class module13class1 extends StatelessWidget {
   const module13class1({super.key});
 
@@ -11,39 +13,8 @@ body: Column(
   mainAxisAlignment:MainAxisAlignment.center,
   children: [
 
-    Stack(
-      children: [
-        Image.network('https://images.pexels.com/photos/1172675/pexels-photo-1172675.jpeg',
-          height:180,
-          width:double.infinity,
-          fit: BoxFit.cover,
-        ),
-        Container(
-          height: 180,
-          color: Colors.black.withOpacity(0.35),
-        ),
-
-
-            Positioned(
-              top: 100,
-              left: 100,
-
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 4),
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(10)
-                ),
-                child: Text('Dhaka',
-                  style: TextStyle(
-                    color: Colors.white,fontSize: 16,
-                  ),
-                ),
-
-              ),
-            ),
-          ],
-        ),
+    CityCard(),
+    CityCard(),
 
 
 
@@ -59,3 +30,5 @@ body: Column(
     );
   }
 }
+
+
