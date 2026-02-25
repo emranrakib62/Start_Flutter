7bbbc17c34ead5b197d6c33808c2ac29917c5e7a
Emranul Hoque <emranrakib01862@gmail.com>
2026-02-25 13:36:45 +0600

    updated

--- lib/Module13/Widget/City_card.dart
+++ lib/Module13/Widget/City_card.dart
@@ -1,45 +1,61 @@
 import 'package:flutter/material.dart';
 
 class CityCard extends StatelessWidget {
-  final String img,CityName;
+  final String img,CityName,rating;
   const CityCard({
-    super.key, required this.img, required this.CityName,
+    super.key, required this.img,  required this.rating, required this.CityName,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.network(img,
-          height:200,
-          width:300,
-          fit: BoxFit.cover,
-        ),
-        Container(
-          height: 180,
-          color: Colors.black.withOpacity(0.35),
-        ),
+    return SizedBox(
+width: 300,
+      height: 200,
 
-
-        Positioned(
-          top: 100,
-          left: 100,
-
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 4),
-            decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(10)
-            ),
-            child: Text(CityName,
-              style: TextStyle(
-                color: Colors.white,fontSize: 16,
-              ),
-            ),
-
+      child: Stack(
+        children: [
+          Image.network(img,
+            height:200,
+            width:300,
+            fit: BoxFit.cover,
           ),
-        ),
-      ],
+          Container(
+            height: 180,
+            color: Colors.black.withOpacity(0.35),
+          ),
+
+
+          Row(
+
+            children: [
+              Positioned(
+                top: 100,
+                left: 100,
+
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 4),
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Text(CityName,
+                    style: TextStyle(
+                      color: Colors.white,fontSize: 16,
+                    ),
+                  ),
+
+                ),
+              ),
+Spacer(),
+              Positioned(
+                right: 12,
+                  bottom: 12,
+                  child: Text('\u2B50 ${rating}'))
+
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/Module13/class_1.dart
+++ lib/Module13/class_1.dart
@@ -16,16 +16,16 @@ SingleChildScrollView(
   scrollDirection: Axis.horizontal,
   child: Row(
     children: [
-      CityCard(img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9_eZd8Gd5dqE-wmWnz7kWp4dZ1K3LLV_jQg&s',CityName: 'Dhaka',),
+      CityCard(img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9_eZd8Gd5dqE-wmWnz7kWp4dZ1K3LLV_jQg&s',CityName: 'Dhaka',rating: '4.8',),
       SizedBox(
         width: 10 ,
       ),
-      CityCard(img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ioiTYLP2SuudZ0QxylhBo02kl9d5vUqihw&s',CityName: 'Chittagong',),
+      CityCard(img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ioiTYLP2SuudZ0QxylhBo02kl9d5vUqihw&s',CityName: 'Chittagong',rating: '5.8',),
 
       SizedBox(
         width: 10 ,
       ),
-      CityCard(img: 'https://i0.wp.com/fromsunrisetosunset.com/wp-content/uploads/2018/10/20180821_113713_4mp_comp-1-scaled.jpg?fit=1024%2C576&ssl=1',CityName: 'Sylet',),
+      CityCard(img: 'https://i0.wp.com/fromsunrisetosunset.com/wp-content/uploads/2018/10/20180821_113713_4mp_comp-1-scaled.jpg?fit=1024%2C576&ssl=1',CityName: 'Sylet',rating: '6.8',),
 
 
 
