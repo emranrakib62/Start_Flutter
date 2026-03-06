581e1e5108647e8c7c02b390d313e30acfe1e790
Emranul Hoque <emranrakib01862@gmail.com>
2026-03-06 12:22:23 +0600

    updated

--- lib/Module14/class_3.dart
+++ lib/Module14/class_3.dart
@@ -9,6 +9,33 @@ class module14class3 extends StatefulWidget {
 class _module14class3State extends State<module14class3> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar:AppBar(
+          title:Text('Tabbar'),
+          bottom: TabBar(
+              tabs:[
+                Tab(
+                  icon: Icon(Icons.home),
+                  text: 'Home',
+                ),
+      
+                Tab(
+                  icon: Icon(Icons.favorite),
+                  text: 'fav',
+                ),
+      
+                Tab(
+                  icon: Icon(Icons.settings),
+                  text: 'Setting',
+                ),
+      
+              ]
+      
+          ),
+        ),
+      ),
+    );
   }
 }
