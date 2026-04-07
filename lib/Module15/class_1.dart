695d92ab9997d079ad1b355dcd00214a45bfac93
Emranul Hoque <emranrakib01862@gmail.com>
2026-04-07 18:46:45 +0600

    updated

--- lib/Module15/class_1.dart
+++ lib/Module15/class_1.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_projects/Module11/Class_2.dart';
+
+import '../Module11/Class_3.dart';
+import '../Module14/class_2.dart';
+import '../Module14/class_3.dart';
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
 
@@ -7,11 +12,28 @@ class BottomNav extends StatefulWidget {
 }
 
 class _BottomNavState extends State<BottomNav> {
+int _selectedIndex=0;
+  List<Widget> _screen=[
+    Module11Class2(),
+    module14class2(),
+    module14class3(),
+    Module11Class3(),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
+        body:_screen[_selectedIndex],
 
+      bottomNavigationBar: NavigationBar(
+selectedIndex: _selectedIndex,
+          onDestinationSelected: (int index){
+  _selectedIndex=index;
+  setState(() {
+
+  });
+          },
           destinations: [
             NavigationDestination(icon: Icon(Icons.home),
             label:'Home'
