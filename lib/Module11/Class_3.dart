558ff8c4fc9b230ba15a24795ad81df8cfbdf973
Emranul Hoque <emranrakib01862@gmail.com>
2026-04-30 23:05:02 +0600

    updated

--- lib/Module11/Class_3.dart
+++ lib/Module11/Class_3.dart
@@ -8,43 +8,81 @@ class Module11Class3 extends StatelessWidget {
     double screenWidth = screenSize.width;
     double screenHeight = screenSize.height;
 
+    final Orientation orientation=MediaQuery.of(context).orientation;
+
     return Scaffold(
 
       appBar: AppBar(
         title: Text('Class-3'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 150,
-              width: 300,
-              color: Colors.red,
-            ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          orientation==Orientation.landscape? Text("Hello now you are landscape mode",style: TextStyle(fontSize: 20),):SizedBox(),
+          SizedBox(height: 20,),
+          Center(
+            child: orientation==Orientation.portrait? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 150,
+                  width: 300,
+                  color: Colors.red,
+                ),
 
-            Text('Test text',
-              style: TextStyle(
-                  fontSize: 18
-              ),
-            ),
+                Text('Test text',
+                  style: TextStyle(
+                      fontSize: 18
+                  ),
+                ),
 
 
-            Container(
-              height: screenHeight * 0.2,
-              width: screenWidth * 0.8,
+                Container(
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.8,
 
-              color: Colors.blue,
-            ),
+                  color: Colors.blue,
+                ),
 
-            Text('Responsive Test',
-              style: TextStyle(
-                  fontSize: 0.04 * screenWidth
-              ),
+                Text('Responsive Test',
+                  style: TextStyle(
+                      fontSize: 0.04 * screenWidth
+                  ),
+                )
+
+              ],
+            ):Row(
+              children: [
+                Container(
+                  height: 150,
+                  width: 300,
+                  color: Colors.red,
+                ),
+
+                Text('Test text',
+                  style: TextStyle(
+                      fontSize: 18
+                  ),
+                ),
+
+
+                Container(
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.8,
+
+                  color: Colors.blue,
+                ),
+
+                Text('Responsive Test',
+                  style: TextStyle(
+                      fontSize: 0.04 * screenWidth
+                  ),
+                )
+
+              ],
             )
-
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
